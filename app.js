fdad01b6227496aac198f22719974ad3e2db2647
Zamir Markovsky <16.zamir@gmail.com>
2018-03-28 14:08:32 +0000

    render the home page

--- app.js
+++ app.js
@@ -5,15 +5,18 @@ var express    = require("express"),
     
 //SETTINGS---------------------------------------------
 app.set('view engine', 'ejs');
+app.set('views', "/home/ubuntu/workspace/app/views");
 app.use(bodyParser.urlencoded({extended:true}));
 ///////////////////////////////////////////////////////
     
 app.get('/', function(req, res){
-    res.send('The page works'); 
+    res.render("index");
 });
 
 
 
+
+//-----------------------------------------------------
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log('The server is running on port '+process.env.PORT+' and ip '+process.env.IP);
 });
