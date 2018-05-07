496a17bc66cec96b282fdfab4be3e26110edeaf2
Zamir Markovsky <16.zamir@gmail.com>
2018-05-07 05:14:59 +0000

    add age calculations and fixed navbar

--- public/js/register.js
+++ public/js/register.js
@@ -10,5 +10,22 @@ function preview_image(event) {
     document.querySelector('#imageUpload').submit();
 }
 
-// add a function that submits both forms (image before the details) asynchronously.
+// $(document).on("click", "#upload", function() {
+// 	var file_data = $("#avatar").prop("files")[0];   // Getting the properties of file from file field
+// 	var form_data = new FormData();                  // Creating object of FormData class
+// 	form_data.append("file", file_data)              // Appending parameter named file with properties of file_field to form_data
+// 	form_data.append("user_id", 123)                 // Adding extra parameters to form_data
+// 	$.ajax({
+//                 url: "/upload_avatar",
+//                 dataType: 'script',
+//                 cache: false,
+//                 contentType: false,
+//                 processData: false,
+//                 data: form_data,                         // Setting the data attribute of ajax with file_data
+//                 type: 'post'
+//       })
+// })
+
+
+
     
