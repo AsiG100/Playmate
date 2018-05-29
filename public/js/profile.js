16b1a49be0667de68af37cd651f1c25e717a9562
Zamir Markovsky <16.zamir@gmail.com>
2018-05-29 05:01:23 +0000

    replace google api key

--- public/js/profile.js
+++ public/js/profile.js
@@ -2,9 +2,7 @@ function getAge(dateString) {
     var today = Date.now();
     var birthDate = Date.parse(dateString);
     var oneYear = 1000 * 60 * 60 * 24 * 365;
-    console.log(today,' ',birthDate);
     var age = (today - birthDate)/oneYear;
-    console.log(age);
     return age;
 }
 
